df40f2637338d3bf2bc45d26d0a34ad979ece40b
akkoros <akkoros@safaricom.co.ke>
2024-07-12 09:39:44 +0300

    format script and diplay content

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
-document.getElementById('birthday_form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    console.log("am in")
-
-    let birthday = document.getElementById('birthday').value;
-    let gender = document.getElementById('gender').value;
-
-    if (!birthday || !gender) {
-        alert('Please fill the required fields');
-        return;
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('birthday_form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const birthdate = new Date(document.getElementById('birthday').value);
+        let gender = document.getElementById('gender').value;
+        const dayOfWeek = birthdate.getDay(); 
+        console.log("day of the week"+dayOfWeek)
+        let akanName;
+       if(gender == "select")
+       {
+        alert("Please Select Gender ");
+       }
+       else if (gender === 'male') {
+        akanName = akanMaleName(dayOfWeek);
+        } else {
+        akanName = akanFemaleName(dayOfWeek);
     }
+    finalResult(akanName,gender,dayOfWeek);
 
-    let date = new Date(birthday);
-    let dayOfWeek = date.getDay();
-
-    const maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-    const femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-
-
-    let akanName = gender === 'male' ? maleNames[dayOfWeek] : femaleNames[dayOfWeek];
-
-    document.getElementById('result').textContent = `Your Akan name is ${akanName}`;
-    
+    });
 });
+
+function akanMaleName(dayOfWeek) {
+    const maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+    return maleNames[dayOfWeek];
+}
+
+function akanFemaleName(dayOfWeek) {
+    const femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+    return femaleNames[dayOfWeek];
+}
+
+function finalResult(akanName,gender,dayOfWeek) {
+    const resultDiv = document.getElementById('result');
+    let dayName = getDayName(dayOfWeek);
+    resultDiv.innerHTML = `<p> Hello  Your gender is : <strong>${gender} !!!</strong>
+    Your birth day was on  : <strong>${dayName} !!!</strong> and Your Akan name is: <strong>${akanName} !!!</strong></p>`;
+}
+
+function getDayName(dayOfWeek) {
+    switch (dayOfWeek) {
+        case 0:
+            return "Sunday";
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
+        default:
+            return "Invalid day";
+    }
+}
+ 
