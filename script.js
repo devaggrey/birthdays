5ac557e41fcb9c0baf1de884e67b1ea330cf1cb6
akkoros <akkoros@safaricom.co.ke>
2024-07-12 09:34:50 +0300

    created two variables birthday and gender

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 document.getElementById('birthday_form').addEventListener('submit', function(event) {
     event.preventDefault();
     console.log("am in")
+
+    var birthday = document.getElementById('birthday').value;
+    var gender = document.getElementById('gender').value;
 });
